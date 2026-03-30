6f937b132bafb69bff4ae1623d5fc786ba69ddbf
tarundeepakjain <tarundeepakjain@gmail.com>
2026-03-31 00:36:17 +0530

    Updated Auth Context - Logout feature

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import api from "../utils/api";
 import Loader from "../components/Loader";
+
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
@@ -8,23 +9,42 @@ export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
 
   const fetchUser = async () => {
-    
     try {
       const res = await api("get", "user/me/");
       setUser(res.data);
-    } catch(error) {
+    } catch (error) {
       setUser(null);
-      console.error(error);
+    } finally {
+      setLoading(false);
     }
-    setLoading(false);
+  };
+
+  const logout = async () => {
+    try {
+      await api("post", "user/logout/"); 
+    } catch (error) {
+      console.error("Logout error:", error);
+    } finally {
+      setUser(null); 
+    }
   };
 
   useEffect(() => {
     fetchUser();
   }, []);
-if(loading) return <Loader/>
+
+  if (loading) return <Loader />;
+
   return (
-    <AuthContext.Provider value={{ user, setUser, loading }}>
+    <AuthContext.Provider
+      value={{
+        user,
+        setUser,
+        loading,
+        logout,
+        fetchUser, 
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
